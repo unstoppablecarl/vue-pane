1f093aceb38808306c5da05961dfc96a9f1702dd
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-06-07 19:50:50 -0400

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import dts from 'vite-plugin-dts';
-import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
     vue(),
     dts({
       tsconfigPath: './tsconfig.app.json',
-    })
+    }),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'VuePane',
-      fileName: 'vue-pane'
+      fileName: 'vue-pane',
     },
     rolldownOptions: {
       // Avoid bundling Vue into your library package
       external: ['vue'],
       output: {
         globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
-});
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
